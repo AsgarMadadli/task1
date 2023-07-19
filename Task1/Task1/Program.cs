2d759b63fe8151a7fa4be36c11b4363a103f1ab6
AsgarMadadli <asgarmm@code.edu.az>
2023-07-19 04:48:55 -0700

    completed

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1.Models
+{
+    public class Student
+    {
+        private static int counter = 0;
+
+        public Student(string name, string surname, double grade)
+        {
+            Name = name;
+            Surname = surname;
+            Grade = grade;
+
+            Id = counter;
+            counter++;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Grade { get; set; }
+    }
+}
---
+++ Task1/Task1/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Task
+{
+    internal class Student
+    {
+        private string name;
+        private string surname;
+        private double grade;
+
+        public Student(string name, string surname, double grade)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.grade = grade;
+        }
+
+        public int Id { get; internal set; }
+    }
+}
